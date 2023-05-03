ec10102f54f159ccf5a4de9db0e5e8751af1f5e1
Erica S. Francisco <erykatdb@yahoo.com.br>
2023-05-03 13:44:07 -0300

    feat: 🎸 index.js to create objects - instantiating classes

---
+++ index.js
@@ -0,0 +1,11 @@
+import User from "./User.js";
+import Docente from "./Docente.js";
+import Admin from "./Admin.js";
+
+// usando esse index.js para instanciar as classes
+
+const novoUser = new User("Mariana", "mariana@email.com", "2001-01-21");
+console.log(novoUser.exibirInfos());
+
+const novoAdmin = new Admin("Rodrigo", "rodrigo@admin.com", "1980-05-16");
+console.log(novoAdmin.exibirInfos());
